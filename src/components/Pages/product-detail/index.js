683023ffc30be619f2da19d85123e5d93cdc5862
Lind <admin@Lind-2.local>
2023-11-22 22:09:26 -0800

    commit

--- src/components/Pages/product-detail/index.js
+++ src/components/Pages/product-detail/index.js
@@ -5,10 +5,9 @@ import { Rate } from 'antd';
 import { PhoneTwoTone, MailTwoTone, CreditCardTwoTone } from '@ant-design/icons';
 import MapLocation from '../../Map';
 import { Col, Row, Card } from 'antd';
+import Banner from '../../banner';
 import './ProductDetail.css';
 
-
-const desc = ['Terrible', 'Bad', 'Normal', 'Good', 'Wonderful'];
 function ProductDetail() {
     const location = {
         address: '1600 Amphitheatre Parkway, Mountain View, california.',
@@ -17,31 +16,7 @@ function ProductDetail() {
     }
     return (
         <div>
-            <div className='banner'>
-                <div className='overlay'></div>
-                <div className='header'>
-                    <h1 className='header-name'>
-                        House in  Financial District
-                        <FontAwesomeIcon icon={faCheck} style={{ marginLeft: '8px' }} />
-                    </h1>
-                    <div className='header-address'>
-                        <FontAwesomeIcon icon={faLocationDot} />
-                        <span className='ml-2 text-white mr-2 address'>70 Bright St New York, USA</span>
-                        <div className='rate-header'>
-                            <Rate disabled value={4} />
-                            <span className="rate-text">{desc[4 - 1]}</span>
-                        </div>
-                    </div>
-                    <div className='prices-date'>
-                        <div className='prices'>
-                            Price: $ <span>50.500</span>
-                        </div>
-                        <div className='date'>
-                            Date: 20.05.2020
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Banner/>
             <div className='detail-container'>
                 <div className='content-detail'>
                     <Row
